2db156e53929a48a9fba60344b6e51525b0a630c
OLAYIIMIIKA <akinbuwaemmanuel6.com>
2023-02-07 01:09:30 +0100

    first commit

---
+++ src/components/LoginForm.js
@@ -0,0 +1,36 @@
+import React, { useState } from "react";
+import "./LoginForm.css"
+import Container from 'react-bootstrap/Container';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form'
+import FormCheckInput from "react-bootstrap/esm/FormCheckInput";
+
+
+
+const LoginForm = () => {
+
+    return (
+      
+        <Container className="page" fluid>
+            <Form  className="cover">
+              
+            <img src="https://user-images.githubusercontent.com/76559183/110728268-b2e43d00-8257-11eb-838f-d52cd6197d08.png" alt="git"/>
+            
+            <h1>Sign in</h1>
+            <lebel htmlFor='email'>Username:</lebel>
+            <input type="text" placeholder="Enter username"/>
+            <lebel htmlFor='password'>Password:</lebel>
+            <input type="password" placeholder="Enter password"/>
+            <Form.Check label="Remeber me"/>
+            <a href="#">Forget password ?</a>
+           
+            <Button variant="danger">Sign in</Button>
+            <p>Don't have an account? <a  href="#">Sign up</a> </p>
+            </Form>
+
+        </Container>
+         
+    );
+}
+
+export default LoginForm 
